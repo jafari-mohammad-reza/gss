dff4e3710887bb2ffee2d5ebef94513706465128
mohammadrezajafari <mohammadrezajafari.dev@gmail.com>
2025-10-24 13:45:30 +0330

    feat: add command parser like redis RESP protocol

--- src/p2p/connection.rs
+++ src/p2p/connection.rs
@@ -2,7 +2,6 @@ use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
 };
-
 use tokio::{io::AsyncReadExt, net::TcpListener};
 use tokio_util::sync::CancellationToken;
 pub struct Server {
@@ -65,38 +64,79 @@ impl Server {
         println!("Server stopped");
     }
     async fn handle_connection(
-        mut _socket: tokio::net::TcpStream,
-        _cancel_token: Arc<CancellationToken>,
+        mut socket: tokio::net::TcpStream,
+        cancel_token: Arc<CancellationToken>,
     ) {
-        let mut buf = [0u8; 1024];
-        tokio::select! {
-                _ = _cancel_token.cancelled() => {
-                    println!("Connection handler stopping due to cancellation");
-                    return;
-                }
-            res = _socket.read(&mut buf) => {
-                    match res {
-                        Ok(n) if n == 0 => {
-                            println!("Connection closed by peer");
-                            return;
-                        }
-                        Ok(n) => {
-                            let bytes = &buf[..n];
-                            match std::str::from_utf8(bytes) {
-                                Ok(s) => {
-                                    println!("Received: {}", s);
-                                }
-                                Err(e) => {
-                                    eprintln!("Failed to parse UTF-8: {:?}", e);
-                                }
-                            }
-                        }
-                        Err(e) => {
-                            eprintln!("Failed to read from socket: {:?}", e);
-                            return;
-                        }
+        let mut buf = vec![0u8; 1024];
+        let mut read_acc: Vec<u8> = Vec::new();
+
+        loop {
+            tokio::select! {
+                _ = cancel_token.cancelled() => return,
+                res = socket.read(&mut buf) => {
+                    let n = match res {
+                        Ok(0) => { println!("Connection closed by peer"); return; }
+                        Ok(n) => n,
+                        Err(e) => { eprintln!("read error: {:?}", e); return; }
+                    };
+
+                    read_acc.extend_from_slice(&buf[..n]);
+
+                    while let Some((consumed, command)) = parse_command(&read_acc) {
+                        read_acc.drain(..consumed);
+                        println!("parsed {:?}", command);
+
+                    }
                 }
             }
         }
     }
+}
+
+#[derive(Debug)]
+struct Command {
+    pub name: String,
+    pub key: String,
+    pub args: HashMap<String, Vec<String>>,
+}
+fn parse_command(buf: &[u8]) -> Option<(usize, Command)> {
+    if buf.is_empty() || buf[0] != b'*' {
+        return None; // invalid or empty buffer for parsing to command
+    }
+    let mut pos = 1; // current possition of readed buf
+    let newline_pos = buf.iter().position(|&b| b == b'\r')?;
+    let arr_len: usize = str::from_utf8(&buf[pos..newline_pos]).ok()?.parse().ok()?;
+    pos = newline_pos + 2; // skip \r\n
+    let mut elements = Vec::with_capacity(arr_len);
+    for _ in 0..arr_len {
+        if buf.get(pos)? != &b'$' {
+            return None;
+        } // must be bulk string
+        pos += 1;
+
+        let newline_pos = buf[pos..].iter().position(|&b| b == b'\r')? + pos;
+        let str_len: usize = str::from_utf8(&buf[pos..newline_pos]).ok()?.parse().ok()?;
+        pos = newline_pos + 2;
+        if buf.len() < pos + str_len + 2 {
+            return None;
+        } // not enough data yet
+        let s = str::from_utf8(&buf[pos..pos + str_len]).ok()?.to_string();
+        elements.push(s);
+        pos += str_len + 2;
+    }
+    if elements.len() < 2 {
+        return None;
+    }
+    let name = elements[0].clone();
+    let key = elements[1].clone();
+    let mut args = HashMap::new();
+
+    for arg in &elements[2..] {
+        if let Some((k, v)) = arg.split_once('=') {
+            let vals = v.split(',').map(|s| s.to_string()).collect::<Vec<_>>();
+            args.insert(k.to_string(), vals);
+        }
+    }
+
+    Some((pos, Command { name, key, args }))
 }
